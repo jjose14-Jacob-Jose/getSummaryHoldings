bb2125a0b9b28a4a4626309d3880f182cd29529a
Shirayuki <gul.prannu@gmail.com>
2023-08-16 23:30:24 -0700

    Created a home page for displaying tool related information to new users.

--- src/app/home/Home.jsx
+++ src/app/home/Home.jsx
@@ -2,30 +2,28 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import InputsCard from "../../components/InputsCard";
-import GeneratedResults from "../../components/GeneratedResultsTable";
-import GeneratedResultsTable from "../../components/GeneratedResultsTable";
-import { useEffect } from "react";
-import { main } from "../../../public/main";
-import Script from "next/script";
-import SummaryHoldings from "../../components/SummaryHoldings";
 
+/**
+ * Home page component of the site. Describes the tool's purpose, features, etc.
+ * 
+ * @author pdoddi
+ */
 export default function Home() {
 
     return (
-        <main className="items-center align-middle justify-center">
+        <div className="items-center align-middle justify-center">
   
-        <div className="p-12  flex gap-4 mx-auto justify-center items-center mb-24">
-                <div className="w-1/2">
+        <div className="p-12  flex gap-24 mx-auto justify-center items-center mb-24">
+                <div className="w-2/5">
                     <h1 className="font-bold text-6xl pb-4 leading-tight">Get Summary Holdings</h1>
-                    <p>A tool to compute summary holdings of books easily for you.</p>
+                    <p>A free browser based solution that computes summary holdings of books for you.</p>
                   
                     <div className="flex gap-4 mt-8">
-                        <Link href="/" className="p-2 px-10 h-fit font-light bg-[#907360] text-white  rounded-xl hover:bg-[#89634A] hover:font-light items-center">
-                            Get Started
+                        <Link href="/" className="p-2 px-10 h-fit font-light bg-[#907360] text-white  rounded hover:bg-[#89634A] hover:font-light items-center">
+                            Start Using It
                         </Link>
-                        <Link href="mailto:support@getsummaryholdings.com" className="p-2 px-8 h-fit font-light text-white hover:text-[#2A2C32] border rounded-xl hover:bg-white hover:font-light items-center">
-                            Read the User Guide
+                        <Link href="mailto:support@getsummaryholdings.com" className="p-2 h-fit font-light text-white hover:underline underline-offset-4  items-center">
+                            Learn More
                         </Link>
                     </div>
                 </div>
@@ -40,16 +38,6 @@ export default function Home() {
                 </div>
             </div> 
             {/* user guide */}
-            {/* <div className="bg-[#907360] py-12">
-                <InputsCard />
-            </div> */}
-            {/* <div id="divMatrix"></div>
-            <GeneratedResultsTable />
-            <SummaryHoldings />
-
-            <script>
-                main();
-            </script>  */}
-        </main>
+        </div>
     )
 }
