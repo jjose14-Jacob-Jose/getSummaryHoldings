b682ea1d794c27668fd0ba4e1a77dbe109d60b10
Shirayuki <gul.prannu@gmail.com>
2023-08-18 16:10:29 -0700

    Added style constants.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { styles } from "@/constants/styles";
 
 /**
  * Home page component of the site. Describes the tool's purpose, features, etc.
@@ -20,10 +21,10 @@ export default function Home() {
     return (
         <div className="items-center align-middle justify-center">
             {/* Hero section */}
-            <div className="p-12  flex gap-24 mx-auto justify-center items-center mb-24">
+            <div className="p-12 flex gap-24 mx-auto justify-center items-center mb-24">
                     <div className="w-1/2">
-                        <h1 className="font-normal text-6xl pb-4 leading-tight">Serials Calculations. Simplified.</h1>
-                        <p className="text-lg">A free browser based solution that computes summary holdings of books for you.</p>
+                        <h1 className={`${styles.headingOne} pb-4`}>Serials Calculations. Simplified.</h1>
+                        <p className={`${styles.paraTwo}`}>A free browser based solution that computes summary holdings of books for you.</p>
                     
                         <div className="flex gap-4 mt-8">
                             <Link href="/" className="p-2 px-10 h-fit font-light bg-[#89634A] text-white text-sm rounded hover:bg-[#83593D] hover:font-light items-center">
@@ -44,10 +45,11 @@ export default function Home() {
                 </div> 
             {/* Learn More */}
             <div id="learnMoreSection">
+                {/* Goodbye to taxing calculations section */}
 
                 {/* Work faster section */}
-                <div className="p-12 mx-auto justify-center items-center">
-                    <h2 className="text-center font-normal text-5xl pb-28">Work Faster by Getting Started</h2>
+                <div className="p-12 mx-auto justify-center items-center pt-28 pb-28">
+                    <h2 className={`text-center ${styles.headingTwo} pb-28`}>Work Faster by Getting Started</h2>
                     <div className="flex gap-12 align-middle items-center mx-auto justify-center mb-24">
                         <Image 
                             src="/Logo draft 1.png"
@@ -57,14 +59,14 @@ export default function Home() {
                             className="rounded-lg drop-shadow-lg"
                         />
                         <div className="w-3/5 align-middle">
-                            <h3 className="font-medium text-3xl">Fill out the details of the serial</h3>
-                            <p className="text-lg">Fill in details like the range of years of the book, the number of editions it has per year, etc. to get a detailed list of editions, issues, and years.</p>
+                            <h3 className={`${styles.headingThree}`}>Fill out the details of the serial</h3>
+                            <p className={`${styles.paraTwo}`}>Fill in details like the range of years of the book, the number of editions it has per year, etc. to get a detailed list of editions, issues, and years.</p>
                         </div>
                     </div>
                     <div className="flex gap-12 align-middle items-center mx-auto justify-center mb-24">
                         <div className="w-3/5 align-middle">
-                            <h3 className="font-medium text-3xl">Select the available issues</h3>
-                            <p className="text-lg">Check the available issues of the serial and mark them in the generated edition details.</p>
+                            <h3 className={`${styles.headingThree}`}>Select the available issues</h3>
+                            <p className={`${styles.paraTwo}`}>Check the available issues of the serial and mark them in the generated edition details.</p>
                         </div>
                         <Image 
                             src="/Logo draft 1.png"
@@ -74,7 +76,7 @@ export default function Home() {
                             className="rounded-lg drop-shadow-lg"
                         />
                     </div>
-                    <div className="flex gap-12 align-middle items-center mx-auto justify-center mb-24">
+                    <div className="flex gap-12 align-middle items-center mx-auto justify-center">
                         <Image 
                             src="/Logo draft 1.png"
                             alt="Portfolio Logo"
@@ -83,11 +85,19 @@ export default function Home() {
                             className="rounded-lg drop-shadow-lg"
                         />
                         <div className="w-3/5 align-middle">
-                            <h3 className="font-medium text-3xl">Get summary holdings data</h3>
-                            <p className="text-lg">Switch between <span className="inline font-medium">basic</span> and <span className="inline font-medium">detailed</span> views based on your need.</p>
+                            <h3 className={`${styles.headingThree}`}>Get summary holdings data</h3>
+                            <p className={`${styles.paraTwo}`}>Switch between <span className="inline font-medium">basic</span> and <span className="inline font-medium">detailed</span> views based on your need.</p>
                         </div>
                     </div>
                 </div>
+
+                {/* Features section */}
+                <div className="p-12 mx-auto justify-center items-center bg-[#1E2027] pt-28 pb-28">
+                    <h2 className={`text-center ${styles.headingTwo} pb-28`}>Explore Additional Features</h2>
+                    
+                </div>
+
+                {/* Contact section */}
             </div>
         </div>
     )
---
+++ src/constants/styles.js
@@ -0,0 +1,8 @@
+export const styles = {
+    headingOne: 'font-normal text-6xl leading-tight',
+    headingTwo: 'font-normal text-5xl',
+    headingThree: 'font-medium text-3xl',
+    paraOne: 'text-2xl',
+    paraTwo: 'font-thin text-lg',
+    paraThree: 'font-thin text-sm',
+}
