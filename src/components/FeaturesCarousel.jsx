2cab619502c7cce0a2c9298cced165d986a5b101
Shirayuki <gul.prannu@gmail.com>
2023-08-18 18:03:20 -0700

    Updated opacity.

--- src/components/FeaturesCarousel.jsx
+++ src/components/FeaturesCarousel.jsx
@@ -39,28 +39,28 @@ export default function FeaturesCarousel({itemsData}) {
                 <div className="w-1/2 space-y-8">
                     <button onClick={() => jumpToItem(0)} className="flex gap-4 items-center text-left">
                         <div className={`h-[70px] w-[1px] bg-white ${currentIndex === 0 ? `opacity-100` : `opacity-40`} transition-all duration-200`}></div>
-                        <div className={`${currentIndex === 0 ? `opacity-100` : `opacity-60`} transition-all duration-200`}>
+                        <div className={`${currentIndex === 0 ? `opacity-100` : `opacity-40`} transition-all duration-200`}>
                             <h3 className={`${styles.paraOne}`}>{itemsData[0].title}</h3>
                             <p>{itemsData[0].desc}</p>
                         </div>
                     </button>
                     <button onClick={() => jumpToItem(1)} className="flex gap-4 items-center text-left">
                         <div className={`h-[70px] w-[1px] bg-white ${currentIndex === 1 ? `opacity-100` : `opacity-40`} transition-all duration-200`}></div>
-                        <div className={`${currentIndex === 1 ? `opacity-100` : `opacity-60`} transition-all duration-200`}>
+                        <div className={`${currentIndex === 1 ? `opacity-100` : `opacity-40`} transition-all duration-200`}>
                             <h3 className={`${styles.paraOne}`}>{itemsData[1].title}</h3>
                             <p>{itemsData[1].desc}</p>
                         </div>
                     </button>
                     <button onClick={() => jumpToItem(2)} className="flex gap-4 items-center text-left">
                         <div className={`h-[70px] w-[1px] bg-white ${currentIndex === 2 ? `opacity-100` : `opacity-40`} transition-all duration-200`}></div>
-                        <div className={`${currentIndex === 2 ? `opacity-100` : `opacity-60`} transition-all duration-200`}>
+                        <div className={`${currentIndex === 2 ? `opacity-100` : `opacity-40`} transition-all duration-200`}>
                             <h3 className={`${styles.paraOne}`}>{itemsData[2].title}</h3>
                             <p>{itemsData[2].desc}</p>
                         </div>
                     </button>
                     <button onClick={() => jumpToItem(3)} className="flex gap-4 items-center text-left">
                         <div className={`h-[70px] w-[1px] bg-white ${currentIndex === 3 ? `opacity-100` : `opacity-40`} transition-all duration-200`}></div>
-                        <div className={`${currentIndex === 3 ? `opacity-100` : `opacity-60`} transition-all duration-200`}>
+                        <div className={`${currentIndex === 3 ? `opacity-100` : `opacity-40`} transition-all duration-200`}>
                             <h3 className={`${styles.paraOne}`}>{itemsData[3].title}</h3>
                             <p>{itemsData[3].desc}</p>
                         </div>
