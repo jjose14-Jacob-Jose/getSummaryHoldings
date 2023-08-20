45e0a4a0c62316c1ef93da298bf5c5268867ea83
Shivaram Bathini <sbathin5@asu.edu>
2023-08-19 23:22:41 -0700

    Few modifications in styling

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -1,14 +1,15 @@
 /**
  * Common Tailwind styles constants.
- * 
+ *
  * @author pdoddi
  */
 
 export const styles = {
-    headingOne: 'font-normal text-6xl leading-tight',
-    headingTwo: 'font-normal text-5xl',
-    headingThree: 'font-medium text-3xl',
-    paraOne: 'text-xl font-medium',
-    paraTwo: 'font-thin text-lg',
-    paraThree: 'font-thin text-sm',
-}
+  headingOne: "font-normal text-6xl leading-tight",
+  headingTwo: "font-normal text-5xl leading-tight",
+  headingThree: "font-medium text-3xl",
+  headingFour: "font-normal text-3xl",
+  paraOne: "text-xl font-medium",
+  paraTwo: "font-thin text-lg",
+  paraThree: "font-thin text-sm",
+};
