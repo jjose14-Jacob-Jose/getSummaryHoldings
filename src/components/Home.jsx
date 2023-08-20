fc29615c0bd04a5535d88dbfebce5dada6d8f79d
Shirayuki <gul.prannu@gmail.com>
2023-08-19 23:43:54 -0700

    Made Work Faster section responsive. Handled text highlight images for smaller screens. Handled text for smaller screens.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,8 +23,8 @@ export default function Home() {
     return (
         <div className="items-center align-middle justify-center text-center lg:text-left">
             {/* Hero section */}
-            <div className="p-12 lg:flex xl:gap-8 mx-auto justify-center items-center mb-24">
-                    <div className="lg:w-1/2 xl:w-2/5">
+            <div className="p-6 mobile:p-12 lg:flex xl:gap-8 mx-auto justify-center items-center mb-24">
+                    <div className="lg:w-1/2 2xl:w-2/5">
                         <h1 className={`${styles.headingOne} pb-4`}>Serials Calculations. Simplified.</h1>
                         <p className={`${styles.paraTwo}`}>A free browser based solution that computes summary holdings of books for you.</p>
                     
@@ -52,7 +52,7 @@ export default function Home() {
                     <Image 
                         src="/landing_illustration.png"
                         alt="Landing page illustration"
-                        width={560}
+                        width={500}
                         height={480}
                         className="mx-auto lg:hidden"
                     />
@@ -62,31 +62,38 @@ export default function Home() {
                 {/* Goodbye to taxing calculations section */}
 
                 {/* Work faster section */}
-                <div className="p-12 mx-auto justify-center items-center pt-28 pb-28">
+                <div className="p-6 mobile:p-12 mx-auto justify-center items-center pt-28 pb-28">
                     <div className="flex mx-auto justify-center">
                         <h2 className={`text-center ${styles.headingTwo}`}>Work Faster by Getting Started</h2>
                         <img 
                             src="/text_highlight.png"
                             alt="Text highlight icon"
-                            className="w-[15px] h-[30px] rotate-[140deg] -mt-5"
+                            className="hidden sm:block w-[15px] h-[30px] rotate-[140deg] -mt-5"
                         />
                     </div>
-                    <div className="flex gap-16 align-middle items-center mx-auto justify-center mt-28">
+                    <div className="lg:flex gap-16 align-middle items-center mx-auto justify-center mt-28">
                         <Image 
                             src="/tool_inputs.png"
                             alt="App Snapshot - fill out serial details"
                             width={500}
                             height={200}
-                            className="rounded-[20px] shadow-[0px_0px_40px_rgba(0,0,0,0.4)] h-auto"
+                            className="mx-auto mb-8 lg:mb-0 lg:mx-0 rounded-[20px] shadow-[0px_0px_40px_rgba(0,0,0,0.4)] h-auto"
                         />
-                        <div className="w-2/5 align-middle">
-                            <h3 className={`${styles.headingThree}`}>Fill out the details of the serial</h3>
+                        <div className="lg:w-2/5 align-middle">
+                            <h3 className={`${styles.headingThree} mb-4 lg:mb-0`}>Fill out the details of the serial</h3>
                             <p className={`${styles.paraTwo}`}>Fill in details like the range of years of the book, the number of editions it has per year, etc. to get a detailed list of editions, issues, and years.</p>
                         </div>
                     </div>
-                    <div className="flex gap-16 w-fit align-middle items-center mx-auto justify-center my-32">
-                        <div className="w-2/5">
-                            <h3 className={`${styles.headingThree}`}>Select the available issues</h3>
+                    <div className="lg:flex gap-16 w-fit align-middle items-center mx-auto justify-center my-32">
+                        <Image 
+                            src="/tool_issue_selection.png"
+                            alt="App Snapshot - select issues"
+                            width={500}
+                            height={200}
+                            className="lg:hidden mx-auto mb-8 lg:mb-0 lg:mx-0 rounded-[20px] shadow-[0px_0px_40px_rgba(0,0,0,0.4)] drop-shadow-lg h-auto"
+                        />
+                        <div className="lg:w-2/5">
+                            <h3 className={`${styles.headingThree} mb-4 lg:mb-0`}>Select the available issues</h3>
                             <p className={`${styles.paraTwo}`}>Check the available issues of the serial and mark them in the generated edition details.</p>
                         </div>
                         <Image 
@@ -94,35 +101,35 @@ export default function Home() {
                             alt="App Snapshot - select issues"
                             width={500}
                             height={200}
-                            className="rounded-[20px] shadow-[0px_0px_40px_rgba(0,0,0,0.4)] drop-shadow-lg h-auto"
+                            className="hidden lg:block rounded-[20px] shadow-[0px_0px_40px_rgba(0,0,0,0.4)] drop-shadow-lg h-auto"
                         />
                     </div>
-                    <div className="flex gap-16 align-middle items-center mx-auto justify-center">
+                    <div className="lg:flex gap-16 align-middle items-center mx-auto justify-center">
                         <Image 
                             src="/tool_generate_summary.png"
                             alt="App Snapshot - get summary holdings"
                             width={500}
                             height={200}
-                            className="rounded-[20px] shadow-[0px_0px_40px_rgba(0,0,0,0.4)] h-auto"
+                            className="mx-auto mb-8 lg:mb-0 lg:mx-0 rounded-[20px] shadow-[0px_0px_40px_rgba(0,0,0,0.4)] h-auto"
                         />
-                        <div className="w-2/5 align-middle">
-                            <h3 className={`${styles.headingThree}`}>Get summary holdings data</h3>
+                        <div className="lg:w-2/5 align-middle">
+                            <h3 className={`${styles.headingThree} mb-4 lg:mb-0`}>Get summary holdings data</h3>
                             <p className={`${styles.paraTwo}`}>Switch between <span className="inline font-medium">basic</span> and <span className="inline font-medium">detailed</span> views based on your need.</p>
                         </div>
                     </div>
                 </div>
 
                 {/* Features section */}
-                <div className="p-12 mx-auto justify-center items-center bg-[#1E2027] pt-28 pb-28">
+                <div className="p-6 mobile:p-12 mx-auto justify-center items-center bg-[#1E2027] pt-28 pb-28">
                     <div className="w-fit mx-auto justify-center">
                         <h2 className={`${styles.headingTwo} pl-[15px]`}>Explore Additional Features</h2>
                         <img 
                             src="/text_highlight.png"
                             alt="Text highlight icon"
-                            className="w-[15px] h-[30px] -rotate-[50deg] -mt-2"
+                            className="hidden sm:block w-[15px] h-[30px] -rotate-[50deg] -mt-2"
                         />
                     </div>
-                    <div className="pt-28">
+                    <div className="pt-12 sm:pt-28">
                         <FeaturesCarousel itemsData={features_list} />
                     </div>
                 </div>
