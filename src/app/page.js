3d3c8921b20623c0ccad93e7f128cc74e8997f11
Shivaram Bathini <sbathin5@asu.edu>
2023-09-10 22:15:22 -0700

    t-54-Added home url

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,8 @@ import Header from '@/components/static/Header'
 import InputsWrapper from '@/components/wrappers/InputsWrapper'
 import ResultsWrapper from '@/components/wrappers/ResultsWrapper'
 
+
+
 /**
  * Server component for the landing page of the site. Displays the tool.
  * 
@@ -12,11 +14,9 @@ import ResultsWrapper from '@/components/wrappers/ResultsWrapper'
  */
 export default function LandingPage() {
 	return (
-		<main className='max-w-[1500px] m-auto min-h-screen flex flex-col'>
+		<main className='m-auto min-h-screen flex flex-col'>
             <Header />
-			<BackToTopButton />
-            <InputsWrapper/>
-			<Footer />
+            <ResultsWrapper />
         </main>
 		// <main className='m-auto min-h-screen flex flex-col'>
 		// 	<Header />
--- src/components/wrappers/ResultsWrapper.jsx
+++ src/components/wrappers/ResultsWrapper.jsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import UserInputs from "../UserInputs";
 import ResultsSection from "../ResultsSection";
+import Footer from "../static/Footer";
+
 
 /**
  * A wrapper component for results to be used for displaying results table conditionally.
@@ -15,11 +17,29 @@ export default function ResultsWrapper() {
     const [editionRows, setEditionRows] = useState(null);
     
     return (
-        <div className="items-center align-middle justify-center p-6 px-12 m-auto w-fit">
+        <div className="m-auto">
+            <div>
+            {displayResults === false && (
+            <div id="userInputs" className="m-auto items-center align-middle justify-center text-center">
+            <div className="pb-4">
+                <h1 className="font-semibold text-3xl pb-1 pl-2">Calculate Editions</h1>
+                <p className="font-light pl-2 text-[#ECECEC]">Fill out the details of the title you wish to generate summary holdings for.</p>
+            </div>
+            </div>
+          )}
+            </div>
+            <div className="items-center align-middle justify-center p-6 px-12 m-auto w-fit">
             <UserInputs setDisplayResults={setDisplayResults} setEditionRows={setEditionRows} />
             {
                 displayResults && <ResultsSection editionRows={editionRows} />
             }
+            </div>
+            <div>
+            {displayResults && (
+            <Footer/>
+          )}
+            </div>
         </div>
+        
     )
 }
