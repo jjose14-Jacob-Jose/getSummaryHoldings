c9731a22a0112523e8098bc103580811efa09d87
Shirayuki <gul.prannu@gmail.com>
2023-09-12 22:36:45 -0700

    SCG-112: Handled conditional display of user input text and results section. Added back to top button as well as footer.

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Header from '@/components/static/Header'
-import InputsWrapper from '@/components/wrappers/InputsWrapper'
+import ResultsWrapper from '@/components/wrappers/ResultsWrapper'
 
 /**
  * Server component for the landing page of the site. Displays the tool.
@@ -10,17 +10,7 @@ export default function LandingPage() {
 	return (
 		<main className='m-auto min-h-screen flex flex-col max-w-[1500px]'>
             <Header />
-            <InputsWrapper/>
+            <ResultsWrapper/>
         </main>
-		// <main className='m-auto min-h-screen flex flex-col'>
-		// 	<Header />
-		// 	<div className='m-auto'>
-		// 		<InputsWrapper />
-		// 		<ResultsWrapper />
-		// 		<script>
-        //         	main();
-        //    	 	</script> 
-		// 	</div>
-		// </main>
 	)
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 "use client";
 
-import HeroSection from "./static/Home/HeroSection";
-import GoodbyeToTaxingCalSection from "./static/Home/GoodbyeToTaxingCalSection";
-import WorkFasterSection from "./static/Home/WorkFasterSection";
-import CheckoutTheProductPrototypeSection from "./static/Home/CheckoutTheProductPrototypeSection";
-import ContactUsSection from "./static/Home/ContactUsSection";
-import FeaturesSection from "./static/Home/FeaturesSection";
+import HeroSection from "./static/home/HeroSection";
+import GoodbyeToTaxingCalSection from "./static/home/GoodbyeToTaxingCalSection";
+import WorkFasterSection from "./static/home/WorkFasterSection";
+import CheckoutTheProductPrototypeSection from "./static/home/CheckoutTheProductPrototypeSection";
+import ContactUsSection from "./static/home/ContactUsSection";
+import FeaturesSection from "./static/home/FeaturesSection";
 
 /**
  * Home page component of the site. Describes the tool's purpose, features, etc.
@@ -13,12 +13,6 @@ import FeaturesSection from "./static/Home/FeaturesSection";
  * @author pdoddi
  */
 export default function Home() {
-  function scrollToLearnMore() {
-    const element = document.getElementById("learnMoreSection");
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  }
 
   return (
     <div className="items-center align-middle justify-center text-center lg:text-left">
