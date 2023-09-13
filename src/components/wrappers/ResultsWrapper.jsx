bd33e5cfa0fcfc2532c6607436cff38903d0fa8e
Shirayuki <gul.prannu@gmail.com>
2023-09-13 10:54:13 -0700

    SCG-120: Moved the inputs up a little more to the center.

--- src/components/wrappers/ResultsWrapper.jsx
+++ src/components/wrappers/ResultsWrapper.jsx
@@ -17,23 +17,25 @@ export default function ResultsWrapper() {
     
     return (
         <>
-            <div className="items-center align-middle justify-center my-auto">
-                {
-                    !displayResults && (
-                        <div className="text-center mx-auto p-6 px-12 pb-4">
-                            <h1 className="font-semibold text-3xl pb-1">Calculate Editions</h1>
-                            <p className="font-light text-[#ECECEC]">Fill out the details of the title you wish to generate summary holdings for.</p>
-                        </div>
-                    )
-                }
-                <UserInputs 
-                    displayResults={displayResults} 
-                    setDisplayResults={setDisplayResults} 
-                    setEditionRows={setEditionRows} 
-                />
-                {
-                    displayResults && <ResultsSection editionRows={editionRows} />
-                }
+            <div className="m-auto flex flex-col">
+                <div className="items-center align-middle justify-center mb-14">
+                    {
+                        !displayResults && (
+                            <div className="text-center mx-auto p-6 px-12 pb-4">
+                                <h1 className="font-semibold text-3xl pb-1">Calculate Editions</h1>
+                                <p className="font-light text-[#ECECEC]">Fill out the details of the title you wish to generate summary holdings for.</p>
+                            </div>
+                        )
+                    }
+                    <UserInputs 
+                        displayResults={displayResults} 
+                        setDisplayResults={setDisplayResults} 
+                        setEditionRows={setEditionRows} 
+                    />
+                    {
+                        displayResults && <ResultsSection editionRows={editionRows} />
+                    }
+                </div>
             </div>
             {
                 displayResults && <Footer />
