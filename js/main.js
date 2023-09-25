8dc85c6aa3e4f18dbfaa43ff6fc23dae69200c9f
Jacob Jose <jjose14@asu.edu>
2023-09-25 14:45:28 -0700

    20230925_1443 28% SCG41 'Select All' linked to '+' and '-' buttons.

--- js/main.js
+++ js/main.js
@@ -509,6 +509,7 @@ function matrixRowAddOrDelete(mode) {
     }
 
     displayMatrixAsHTMLTable();
+    toggleToggleSwitches();
 }
 // Get last element of the array.
 function arrayGetLastElement(array) {
