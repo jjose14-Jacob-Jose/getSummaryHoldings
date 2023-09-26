578e98d5103e4dadcfa9c5442d879287a0b3b66f
Jacob Jose <jjose14@asu.edu>
2023-09-26 00:48:29 -0700

    SCG_41 Added new-tab logo beside 'Lite' page hyperlink.

--- src/components/static/Header.jsx
+++ src/components/static/Header.jsx
@@ -29,7 +29,9 @@ export default function Header() {
             {/* Help modal button */}
 
             {/*Added Lite page here.    */}
-             <Link href="/lite" rel="noopener noreferrer" target="_blank" className="mt-3 lg:mt-0 p-2 h-fit font-light text-sm text-white hover:underline underline-offset-4  items-center">Lite Version</Link>
+            <Link href="/lite" rel="noopener noreferrer" target="_blank" className="mt-3 lg:mt-0 p-2 h-fit font-light text-sm text-white hover:underline underline-offset-4  items-center">
+                <span style={{ display: 'inline-block' }}>Lite Version <img src="/new_tab_logo.png" alt="New Tab Icon" className="ml-1" style={{ width: '15px', height: 'auto', display: 'inline-block' }} /></span>
+            </Link>
             <Link href="mailto:support@getsummaryholdings.com" rel="noopener noreferrer" target="_blank" className="p-2 px-4 h-fit text-white hover:text-[#2A2C32] border-[1px] font-light rounded  hover:bg-white hover:font-light items-center">
                 Contact Support
             </Link></div>
