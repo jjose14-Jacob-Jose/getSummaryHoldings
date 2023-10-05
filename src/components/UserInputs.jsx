92da4899f2825ac3dfbe686d97ef86fcc6ada6c4
Shirayuki <gul.prannu@gmail.com>
2023-10-04 17:06:26 -0700

    SCG-69: Handled width issue.

--- src/components/UserInputs.jsx
+++ src/components/UserInputs.jsx
@@ -26,12 +26,12 @@ export default function UserInputs({ setDisplayResults, setEditionRows, displayR
     const [startingVolErrorMessage, setStartingVolErrorMessage] = useState("");
     const [issuesErrorMessage, setIssuesErrorMessage] = useState("");
 
+    //Checks for inputs validity in real-time. 
     useEffect(() => {
         validateInputs();
     }, [values]);
 
-
-    // Validates user inputs.
+    //Validates user inputs.
     function validateInputs(){
         //Resetting the states
         setEarliestYearErrorMessage("");
@@ -71,9 +71,7 @@ export default function UserInputs({ setDisplayResults, setEditionRows, displayR
 
     function handleCalculateEditionsClick(e) {
         validateInputs();
-        // console.log(hasAnError());
         if(!hasAnError()){
-            console.log("inside");
             let rows = initializeArrays();
             if(rows.length > 0){
                 setEditionRows(null);
@@ -101,7 +99,6 @@ export default function UserInputs({ setDisplayResults, setEditionRows, displayR
                 <div className="flex gap-6 pr-4">
                     <div className="max-w-[100px]">
                         <label id="lblTxtTextEditionsType" htmlFor="txtTextEditionsType" className="font-medium text-[13px]">Edition type</label>
-                        {/* <input type="text" id="txtTextEditionsType" className="text-black font-light mt-1 p-1 pl-[10px] text-sm border-black/[0.2] border-[1.5px] rounded max-w-[100px]" name="editionsType" required placeholder="vol/ser/no" defaultValue="vol" /> */}
                         <TextField
                             error={editionErrorMessage ? true: false}
                             id="txtTextEditionsType"
@@ -122,9 +119,9 @@ export default function UserInputs({ setDisplayResults, setEditionRows, displayR
                             value={values.editionType}
                         />
                     </div>
+
                     <div className="max-w-[131px]">
                         <label id="lblTxtNumberYearStarting" htmlFor ="txtNumberYearStarting" className="font-medium text-[13px]">Earliest year</label>
-                        {/* <input type="number" min="1" id="txtNumberYearStarting" className="text-black font-light mt-1 p-1 pl-[10px] text-sm border-black/[0.2] border-[1.5px] rounded max-w-[100px]" name="yearStarting" required defaultValue="2000"  /> */}
                         <TextField
                             error={earliestYearErrorMessage ? true: false}
                             id="txtNumberYearStarting"
@@ -146,11 +143,10 @@ export default function UserInputs({ setDisplayResults, setEditionRows, displayR
                             value={values.earliestYear}
                             type="number"
                         />
-                    
                     </div>
+
                     <div className="max-w-[130px]">
                         <label id="lblTxtNumberYearEnding" htmlFor ="txtNumberYearEnding" className="font-medium text-[13px]">Latest year</label>
-                        {/* <input type="number" min="1" id="txtNumberYearEnding" className="text-black font-light mt-1 p-1 pl-[10px] text-sm border-black/[0.2] border-[1.5px] rounded max-w-[100px] text-ellipsis hover:overflow-visible" name="yearEnding" required defaultValue="2020" /> */}
                         <TextField
                             error={latestYearErrorMessage ? true: false}
                             id="txtNumberYearEnding"
@@ -176,7 +172,6 @@ export default function UserInputs({ setDisplayResults, setEditionRows, displayR
 
                     <div className="max-w-[133px] w-fit">
                         <label id="lblTxtNumberEditionsPerYear" htmlFor ="txtNumberEditionsPerYear" className="font-medium text-[13px]">Issues in a year</label>
-                        {/* <input type="number" min="1" id="txtNumberEditionsPerYear" className="text-black mt-1 font-light p-1 pl-[10px] text-sm border-black/[0.2] border-[1.5px] rounded max-w-[133px]" name="editionsPerYear" required defaultValue="12" /> */}
                         <TextField
                             error={issuesErrorMessage ? true: false}
                             id="txtNumberEditionsPerYear"
@@ -201,7 +196,6 @@ export default function UserInputs({ setDisplayResults, setEditionRows, displayR
                     </div>
                     <div className="max-w-[130px]">
                         <label id="lblTxtNumberVolumeStartingYear" htmlFor ="txtNumberVolumeStartingYear" className="font-medium text-[13px]">Starting volume</label>
-                        {/* <input type="number" min="1" className="text-black font-light p-1 pl-[10px] rounded mt-1 max-w-[124px] border-black/[0.2] border-[1.5px] text-sm" id="txtNumberVolumeStartingYear" name="volumeStartingYear" required defaultValue="1"  /> */}
                         <TextField
                             error={startingVolErrorMessage ? true: false}
                             id="txtNumberVolumeStartingYear"
